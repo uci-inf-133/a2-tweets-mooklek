70ce200fbd9edb1fdb1b505e3ee81cd469519e5e
mooklek <michaelykim03@gmail.com>
2025-10-29 02:19:02 -0700

    Completed Tweet Dates task.
    Created dates array by mapping each tweet's time to a Date object. Fetched the earliest and latest dates from the created array. Formatted according to industructions and updated DOM (html) accordingly.

--- js/about.js
+++ js/about.js
@@ -12,6 +12,16 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	if (tweet_array.length > 0) {
+		const dates = tweet_array.map(t => new Date(t.time));
+		const earliestDate = new Date(Math.min(...dates));
+		const latestDate = new Date(Math.max(...dates));
+		const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+		
+		document.getElementById('firstDate').innerText = earliestDate.toLocaleDateString(undefined, options);
+		document.getElementById('lastDate').innerText = latestDate.toLocaleDateString(undefined, options);
+	}
 }
 
 //Wait for the DOM to load
